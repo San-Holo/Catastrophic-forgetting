deefdec97f54e4eff4329cf66ba7ea1554b2c0d0
NicolasBi <nicolasbizzozzero@gmail.com>
2019-01-15 11:23:13 +0100

    Cleaned src

--- src/Conv_archi.py
+++ src/Conv_archi.py
@@ -1,30 +1,20 @@
 import numpy as np
 import pandas as pd
-from random import shuffle
 from math import floor
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data.dataset import Dataset
-from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import transforms
-
-from skimage import io
-from glob import iglob
-
-from PIL import Image
-
-from functools import partial
 
 import itertools as it
 
 from tqdm import tqdm
 
 
-####Model####
-
-def conv_block(in_filter, output_filter, nb_conv, kernel_size=3, activation_function=nn.ReLU()):
+def conv_block(in_filter, output_filter, nb_conv, kernel_size=3,
+               activation_function=nn.ReLU()):
     """To simplify the creation of convolutional sequences
 
     Parameters
@@ -51,11 +41,13 @@ def conv_block(in_filter, output_filter, nb_conv, kernel_size=3, activation_func
     sequential = []
 
     for i in range(nb_conv):
-            sequential.append(nn.Conv2d(nbchannel, nbfilter, kernel_size, padding=1))
-            sequential.append(activation_function)
-            nbchannel = nbfilter
+        sequential.append(
+            nn.Conv2d(nbchannel, nbfilter, kernel_size, padding=1))
+        sequential.append(activation_function)
+        nbchannel = nbfilter
     sequential.append(nn.MaxPool2d(2))
     return sequential
+
 
 def network_from_shape(net_structure, activation=nn.ReLU()):
     """To simplify the creation of fully connected layers sequences
@@ -75,10 +67,11 @@ def network_from_shape(net_structure, activation=nn.ReLU()):
     """
     temp = []
     for prev, next in zip(net_structure[:-1], net_structure[1:]):
-         temp.append(nn.Linear(prev, next))
-         temp.append(activation)
-    temp = temp[:-1] # Remove last activation return temp
+        temp.append(nn.Linear(prev, next))
+        temp.append(activation)
+    temp = temp[:-1]  # Remove last activation return temp
     return temp
+
 
 class Convolutional(nn.Module):
     """A class for convolutional architectures"""
@@ -89,7 +82,9 @@ class Convolutional(nn.Module):
         Parameters
         ----------
         conv_sizes :  int tuple list
-            A list containing a tuple for each convolutionnal layer -> (number of filters, number of convolutions layers, kernel size)
+            A list containing a tuple for each convolutionnal
+            layer -> (number of filters, number of convolutions layers,
+                      kernel size)
 
         img_shape : int couple
             A tuple describing image's height and with
@@ -100,20 +95,27 @@ class Convolutional(nn.Module):
         speed_size : int
             Size of speed one-hot encoding
         """
-        super(Behavior_clone, self).__init__()
+        super(nn.Module, self).__init__()
 
         conv_sizes = [(img_shape[0],)] + conv_sizes
-        layers = it.chain(*[conv_block(prev[0], curr[0], curr[1], curr[2]) for prev, curr in zip(conv_sizes, conv_sizes[1:])])
+        layers = it.chain(*[conv_block(prev[0], curr[0], curr[1], curr[2])
+                            for prev, curr in zip(conv_sizes, conv_sizes[1:])])
         self.conv = nn.Sequential(*layers)
-        #print(self.conv)
+        # print(self.conv)
         img_flatten_size = img_shape[1:]
-        #Use of formula in : https://pytorch.org/docs/stable/nn.html#torch.nn.MaxPool2d to calculate input size for first layer of the fully connected block
-        for i in range(len(conv_sizes)-1):
-            img_flatten_size = floor((img_flatten_size[0] + 2 * 0 - 1 * (2 - 1) - 1) / 2 + 1), floor((img_flatten_size[1] + 2 * 0 - 1 * (2 - 1) - 1) / 2 + 1)
-        img_flatten_size = img_flatten_size[0] * img_flatten_size[1] * conv_sizes[-1][0]
-        self.fc = network_from_shape((img_flatten_size + speed_size,) + network_lin_shape + (2,), activation=nn.ReLU())
+        # Use of formula in :$
+        # https://pytorch.org/docs/stable/nn.html#torch.nn.MaxPool2d
+        # to calculate input size for first layer of the fully connected block
+        for i in range(len(conv_sizes) - 1):
+            img_flatten_size = floor((img_flatten_size[0] + 2 * 0 - 1 * (2 - 1) - 1) / 2 + 1), floor(
+                (img_flatten_size[1] + 2 * 0 - 1 * (2 - 1) - 1) / 2 + 1)
+        img_flatten_size = img_flatten_size[0] * \
+            img_flatten_size[1] * conv_sizes[-1][0]
+        self.fc = network_from_shape(
+            (img_flatten_size + speed_size,) + network_lin_shape + (2,),
+            activation=nn.ReLU())
         self.fc = nn.Sequential(*self.fc)
-        #print(self.fc)
+        # print(self.fc)
 
     def forward(self, img, speed):
         """Passing img and speed input as describe in NVIDIA architecture
@@ -124,10 +126,15 @@ class Convolutional(nn.Module):
             (Height, width, channels)
 
         speed : float Tensor
-            One hot vector -> One hot because if we concatenate just one value to the flattened output ouf convolutional sequence, speed will be not really taken in account for training
+            One hot vector -> One hot because if we concatenate just one
+            value to the flattened output ouf convolutional sequence, speed
+            will be not really taken in account for training
         """
         output = self.conv(img)
-        output = torch.cat((output.reshape(output.size(0),-1), speed), dim=1)
+        output = torch.cat((output.reshape(output.size(0), -1), speed), dim=1)
         output = self.fc(output)
         return output
 
+
+if __name__ == '__main__':
+    pass
---
+++ src/utils.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def import_csv(list_files):
+    """Importing a list of csv files into a pandas Dataframe for further
+    explorations, correcting mirror effect
+
+    Parameters
+    ----------
+    list_file :  String list
+        A list of csv file's name containing all driving informations for
+        multiple users
+
+    Returns
+    ---------
+    dataframes : Dataframe list
+        Converted csv files
+
+    """
+    dataframes = []
+
+    for file in list_files:
+        tmp = pd.read_csv(file)
+        tmp['position_y'] *= -1  # AirSim y axis is inverted compared to
+                                 # matplotlib's
+        dataframes.append(tmp)
+
+    return dataframes
